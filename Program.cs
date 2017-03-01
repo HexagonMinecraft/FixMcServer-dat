54f7107e33556758944cf7b5e5f94baeefb5134b
Noah Prail <nprail@filiosoft.com>
2017-03-01 11:50:00 -0500

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FixMcServer_dat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String McPath = Environment.GetEnvironmentVariable("APPDATA") + "\\.minecraft\\";
+            String McDatTmpPath = McPath + "servers.dat_tmp";
+            String McDatPath = McPath + "servers.dat";
+
+            Console.WriteLine("Are you sure? [y/N]");
+
+            string answer = Console.ReadLine();
+
+            if ((answer == "Y") || (answer == "y"))
+            {
+                if (System.IO.File.Exists(McDatTmpPath))
+                {
+                    Console.WriteLine("Moving from " + McDatTmpPath + " to " + McDatPath);
+                    System.IO.File.Move(McDatTmpPath, McDatPath);
+                }
+                else
+                {
+                    Console.WriteLine("The tmp file does not exist. Looking in " + McDatTmpPath);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You did not answer with y.");
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# FixMcServer-dat
+A simple script that fixes the MC server.dat file. 
+
